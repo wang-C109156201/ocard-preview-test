519e41e742ec04652607e0220e8e63a0c47234dc
ho-chi <60824gi@gmail.com>
2024-05-13 15:03:51 +0800

    home page css

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,89 @@
 <template>
   <div id="app">
-    <Main :propData='initData'/>
+    <div class="linePreview">
+      <div class="bg">
+        <Main :propData='initData'/>
+      </div>
+      <div class="buttonMenu">
+        <img :src="svg" alt="keyboard image">
+        <div class="MenuTextBox">
+          <div class="MenuTextStyle">{{ MenuText }}</div>
+          <img :src="triangle" alt="triangle image" class="triangle">
+        </div>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
 import Main from './components/Main.vue'
+import svg from './assets/keyboard.svg';
+import triangle from './assets/triangle.svg'
+
 export default {
-    data(){
-      return {
-        initData: 'hello 你好'
-      }
-    },
-    components:{
-      Main
+  data(){
+    return {
+      initData: 'hello 你好',
+      svg: svg,
+      triangle: triangle
     }
+  },
+  components:{
+    Main
+  },
+  props: {
+    MenuText: {
+      type: String,
+      default: '選單',
+    }
+  }
 }
 
 </script>
 
 <style>
+.linePreview{
+  border: 0.88px solid #E5E7EB;
+  box-shadow: 0px 0px 10px 0px #0000001A;
+  width: 300px;
+  height:563px;
+  border-radius:14px 14px 14px 14px;
+}
+.bg {
+  width: 300px;
+  height: 519px;
+  background-color: #8DABD8;
+  border-radius:14px 14px 0px 0px;
+}
+.buttonMenu{
+  width: 300px;
+  height: 44px;
+  background-color: white;
+  border-radius:0px 0px 14px 14px;
+  display: flex;
+  align-items: center;
+  color: #606266;
+}
+.MenuTextBox{
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+}
+.triangle{
+  width:5.21px;
+  height:4.93px;
+}
+.buttonMenu > img{
+  margin-left: 16px;
+}
+.MenuTextStyle{
+  font-family: Noto Sans SC;
+  font-size: 12.28px;
+  font-weight: 400;
+  line-height: 15.79px;
+}
+
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,46 +1,20 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="container">
-    <div class="bg">
-      <div>{{ propData }}</div>
-    </div>
-    <div class="buttonMenu">
-      <img :src="svg" alt="keyboard image">
-    </div>
-  </div>
+  <div>{{ propData }}</div>
 </template>
 
 <script lang="ts">
-import svg from "../src/assets/keyboard.svg";
+
 export default {
   props: {
     propData: {
       type: String,
       default: '我是默认值'
     }
-  },
-  data() {
-    return {
-      svg: svg
-    };
   }
 }
 </script>
 
 <style scope>
-.container{
-  border: 0.88px solid #E5E7EB;
-}
-.bg {
-  width: 300px;
-  height: 519px;
-  background-color: #8DABD8;
-  border-radius:14px 14px 0px 0px;
-}
-.buttonMenu{
-  width: 300px;
-  height: 44px;
-  background-color: white;
-  border-radius:0px 0px 14px 14px;
-}
+
 </style>
