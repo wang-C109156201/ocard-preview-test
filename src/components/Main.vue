9fff1a380052a759e8f8d023a2217dac239cce50
ho-chi <60824gi@gmail.com>
2024-05-13 11:57:56 +0800

    基本切版

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,30 +1,46 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div class="container">
-      <div>{{msg}}</div>
-      <div>{{propData}}</div>
+  <div class="container">
+    <div class="bg">
+      <div>{{ propData }}</div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'yyl-npm-practice',
-    data () {
-      return {
-        msg: 'Welcome to Your Vue.js App'
-      }
-    },
-    props: {
-        propData: {
-            type: String,
-            default: '我是默认值'
-        }
+    <div class="buttonMenu">
+      <img :src="svg" alt="keyboard image">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import svg from "../src/assets/keyboard.svg";
+export default {
+  props: {
+    propData: {
+      type: String,
+      default: '我是默认值'
     }
+  },
+  data() {
+    return {
+      svg: svg
+    };
   }
-  </script>
-  
-  <style lang="scss">
-  .container{
-      text-align: center;
-  }
-  </style>
-  
+}
+</script>
+
+<style scope>
+.container{
+  border: 0.88px solid #E5E7EB;
+}
+.bg {
+  width: 300px;
+  height: 519px;
+  background-color: #8DABD8;
+  border-radius:14px 14px 0px 0px;
+}
+.buttonMenu{
+  width: 300px;
+  height: 44px;
+  background-color: white;
+  border-radius:0px 0px 14px 14px;
+}
+</style>
